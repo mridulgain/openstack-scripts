c8087a531544e4b562395ae9f8d8e6f1ab1b4197
Mridul Gain <mridulgain@gmail.com>
2021-06-04 14:48:35 +0530

    backup/restore snat policies

--- backup_restore_snat_policy.sh
+++ backup_restore_snat_policy.sh
@@ -1,22 +1,66 @@
-# 1. getting active snatpolicy names
-# 2. save their config in .yaml 
-# 3. remove/delete them.
-# ---- run snat tests ----
-# 3. re-deploy using the details recorded in step 1.
-
 #!/bin/bash
+# :backup & remove:
+#   getting active snatpolicy names
+#   save their config in .yaml 
+#   remove/delete them.
+# :restore:
+#   re-deploy using the details recorded in step 1.
+
 set -x
-readarray -t snatPolicies < <(kubectl get snatpolicy -A -o name| tail +2)
-declare -p snatPolicies
-for i in ${!snatPolicies[@]}
-do
-    kubectl get snatpolicy ${snatPolicies[$i]} -o yaml > /tmp/snat_$i.yaml
-    kubectl delete snatpolicy ${snatPolicies[$i]}
-done
 
-# command to run snat tests
+backup()
+{
+    readarray -t snatPolicies < <(kubectl get snatpolicy -A -o name| tail +2)
+    declare -p snatPolicies
+    for i in ${!snatPolicies[@]}
+    do
+        kubectl get snatpolicy ${snatPolicies[$i]} -o yaml > ${backup_location}/snat_$i.yaml
+        kubectl delete snatpolicy ${snatPolicies[$i]}
+    done
+}
 
-for i in ${!snatPolicies[@]}
-do
-    kubectl apply -f /tmp/snat_$i.yaml
-done
+apply()
+{
+    for i in $(ls ${restore_location}/snat_*.yaml)
+    do
+        kubectl apply -f $i
+    done
+}
+
+help()
+{
+    echo "Backup snat policies currently running on your cluster"
+    echo "or, apply previously backed up snat policies."
+    echo
+    echo "Usage: $0 [<option> [<path>]]"
+    echo "Options:"
+    echo "-h or --help      Show usage"
+    echo "-b or --backup    Specify backup location"
+    echo "-a or --apply     Specify location from where to restore"
+}
+
+if [[ $# -eq 1  || $# -eq 2 ]]
+then
+    opt=$1
+    case $opt in 
+        -b|--backup)
+        backup_location="/tmp"
+        if [ ! -z $2 ];then backup_location=$2;fi
+        echo "Backing up at: ${backup_location}"
+        backup
+        ;;
+        -a|--apply)
+        restore_location="/tmp"
+        if [ ! -z $2 ];then restore_location=$2;fi
+        echo "Applying from: ${restore_location}"
+        apply
+        ;;
+        -h|--help)
+        help
+        ;;
+        *)
+        echo "Invalid option. Try $0 --help instead."
+    esac
+else
+    echo "Invalid Usage. Try $0 --help instead."
+fi
